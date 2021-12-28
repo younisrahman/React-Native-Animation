bfbef3e6fe26e63de0f27fb52e3eb6721db204a0
Younis Rahman <younisrahman007@gmail.com>
2021-12-28 19:06:30 +0600

    PinchGestureHandlerScreen

---
+++ src/screens/PinchGestureHandlerScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PinchGestureHandlerScreen = () => {
+  return (
+    <View>
+      <Text>Pinch Gesture Handler Screen</Text>
+    </View>
+  );
+};
+
+export default PinchGestureHandlerScreen;
